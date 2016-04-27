069e25f3ea508357d53d0afef8aefb7b3a049e09
Eric Defore <eric.defore@gmail.com>
2016-04-27 09:31:46 -0400

    Added _config_local.yml. Now unless we run gulp with the --production flag, it will build for local. Referencing #3

--- gulp/tasks/jekyll.js
+++ gulp/tasks/jekyll.js
@@ -5,8 +5,13 @@ var isProduction  = require('../util/isProduction');
 var spawn         = require('cross-spawn');
 
 gulp.task('jekyll-build', function(done) {
-  browserSync.notify(config.notification);
-  // Spawn jekyll commands
-  return spawn('bundle', ['exec', 'jekyll', 'build'], {stdio: 'inherit'})
-    .on('close', done);
+    browserSync.notify(config.notification);
+    // Spawn jekyll commands
+    if(isProduction) {
+        return spawn('bundle', ['exec', 'jekyll', 'build'], {stdio: 'inherit'})
+        .on('close', done);
+    } else {
+        return spawn('bundle', ['exec', 'jekyll', 'build', '--config', '_config.yml,_config_local.yml'], {stdio: 'inherit'})
+        .on('close', done);
+    }
 });
